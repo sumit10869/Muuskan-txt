254491c4b99290d9f9f58ea2d96efd46ff4dda2f
sumit10869 <142525697+sumit10869@users.noreply.github.com>
2024-06-10 08:42:50 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 24250238
 api_hash = "cb3f118ce5553dc140127647edcf3720"
-bot_token = "6253605770:AAF5qGr1wfNbdMUV743RjFnRSFfk4hoCaiU"
+bot_token = "7189377174:AAFAy9mdlVhabsidVOPkhLRf9zmBLsfns0w"
 
